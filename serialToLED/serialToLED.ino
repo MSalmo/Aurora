f59d4a656f4bbe631cc89dd59b3ae25ee549fcb7
Malak Salmo <mpsalmo@alumni.csupomona.edu>
2013-06-17 19:04:17 -0700

    Cleanup and Updates
    Changed multiple if-else statements to switch. Fixed not reading from
    serial. It was reading from pin A0 for no reason.

--- serialToLED/serialToLED.ino
+++ serialToLED/serialToLED.ino
@@ -1,34 +1,53 @@
 #include <Adafruit_NeoPixel.h>
 
-Adafruit_NeoPixel strip = Adafruit_Neopixel(240, 6, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(240, 6, NEO_GRB + NEO_KHZ800);
 
 int pinRangeStart = 0;
 int pinRangeStop = 0;
-int inByte;
+char inByte;
+uint8_t ledAddr;
+uint16_t *colorInfo;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
   while(!Serial);
   
-  while(Serial.available() > 0){
-    inByte = analogRead(A0);
-    if(inByte == 'a')
-      pinRangeStart = analogRead(A0);
-    else if(inByte == 'b')
-      pinRangeStop = analogRead(A0);
-    if(pinRangeStart && pinRangeStop){
-      if(pinRangeStop < pinRangeStart){
-        int temp = pinRangeStop;
-        pinRangeStop = pinRangeStart;
-        pinRangeStart = temp;
-      }
-      break;
-    }
-  }
+  Serial.print("Desktop LED Ambience");
   
   strip.begin();
   strip.show();
+  
+  
+
 }
 void loop(){
-  
+while(Serial.available() > 0){
+    inByte = Serial.read();
+    switch(inByte){
+      case 'a': //Set start pin
+        pinRangeStart = Serial.read();
+        break;
+      case 'b': //Set stop pin
+        pinRangeStop = Serial.read();
+        if(pinRangeStart && pinRangeStop){
+          if(pinRangeStop < pinRangeStart){
+            int temp = pinRangeStop;
+            pinRangeStop = pinRangeStart;
+            pinRangeStart = temp;
+          }
+          break;
+        }
+      case 't': //Test case (light a single LED on the strip with a certain color)
+        colorInfo = new uint16_t[3];
+        ledAddr = Serial.read();
+        
+        colorInfo[0] = Serial.read(); //Red
+        colorInfo[1] = Serial.read(); //Green
+        colorInfo[2] = Serial.read(); //Blue
+        
+        strip.setColorAtPixel(ledAddr, strip.Color(colorInfo[0], colorInfo[1], colorInfo[2]));
+        
+        break;
+    }
+  }  
 }
