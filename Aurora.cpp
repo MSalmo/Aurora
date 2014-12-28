03eb2abd3de554022cbd3a8543ae26c0d5be2963
Malak Salmo <mpsalmo@alumni.cpp.edu>
2014-12-28 11:42:42 -0800

    modified:   Aurora.cpp 	Removed any extraenuous code from Aurora.cpp

--- Aurora.cpp
+++ Aurora.cpp
@@ -14,25 +14,7 @@ int main (int argc, char* argv[]) {
 	DesktopManager dm(ARDUINO, DESKTOP_PATH, nLEDs);	
 
 	while(true){
-		uint8_t* testWrite = (uint8_t*)malloc(6);
-			for(uint8_t i = 0; i < 240; i++){
-				testWrite[0] = (uint8_t)'s';
-				testWrite[1] = (uint8_t)rand()%256;
-				testWrite[2] = (uint8_t)rand()%256;
-				testWrite[3] = (uint8_t)rand()%256;
-				testWrite[4] = (uint8_t)i;
-				testWrite[5] = (uint8_t)'z';
-				char* outPrint = (char*)malloc(17);
-				sprintf(outPrint, "R%03dG%03dB%03dL%03d\n",
-					testWrite[1], testWrite[2], testWrite[3], testWrite[4]);
-				fwrite(outPrint,17,1,stdout);
-				//set_rts(arduinoFD, 0);
-				tcdrain(arduinoFD);
-				write(arduinoFD, testWrite, 6);
-				usleep(17.5 * 1000);
-				//set_rts(arduinoFD, 1);
-			}
-			free(testWrite);
+		//TODO: add a loop here to manage the pixel-picking via the DM.
 	}
 
 }
